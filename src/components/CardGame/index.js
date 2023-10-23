77429be283764fe494ac969569d080dab51b73ed
Gustavo Filardi <gustavoafilardi@gmail.com>
2023-10-23 09:11:42 -0300

    :tada: Another program may be editing the same file.
        If this is the case, be careful not to end up with two

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function CardGame(){
+    return`
+    
+    <article class= "card-game">
+       <img  src="imgs/alura-pixel 1.png" alt="logo da Alura">
+    </article>
+    
+    `;
+}
+export default CardGame 
